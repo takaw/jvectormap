2fb5f5c74a1ff91c7e53a574e61ae05d8280aa66
Taka <taka.heliosdesign@gmail.com>
2019-03-20 16:08:59 +0100

    zoomStep undefined fix
    Applied fix as described here: https://github.com/bjornd/jvectormap/issues/415

--- src/map.js
+++ src/map.js
@@ -591,7 +591,7 @@ jvm.Map.prototype = {
     }
 
     if (typeof anchorX != 'undefined' && typeof anchorY != 'undefined') {
-      zoomStep = scale / this.scale;
+      var zoomStep = scale / this.scale;
       if (isCentered) {
         transX = anchorX + this.defaultWidth * (this.width / (this.defaultWidth * scale)) / 2;
         transY = anchorY + this.defaultHeight * (this.height / (this.defaultHeight * scale)) / 2;
